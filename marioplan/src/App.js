1b6731ed55aec3ad9a64003476da3bcb34a5a7e5
Huy <legiahuy1996@outlook.com>
2019-12-05 20:58:07 +0700

    Setting up

---
+++ marioplan/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+          <h>Mario plan</h>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
