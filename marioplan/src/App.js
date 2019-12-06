7be5db3d08c60268f992df92abdf8672b8c3552f
Huy <legiahuy1996@outlook.com>
2019-12-06 22:02:34 +0700

    Navbar component

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/layout/navbar'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-          <h>Mario plan</h>
+          <Navbar></Navbar>
       </div>
     </BrowserRouter>
   );
