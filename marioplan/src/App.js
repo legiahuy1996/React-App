d8524e32c21e901ed2bdb9bce1bcef12cd243298
Huy <legiahuy1996@outlook.com>
2019-12-07 08:03:27 +0700

    Login & Sign Up components

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Switch>
           <Route exact path='/' component={Dashboard}></Route>
           <Route path='/project/:id' component={ProjectDetails}></Route>
+          <Route path='/Signin' component={SignIn}/>
+          <Route path='/SignUp' component={SignUp}/>
         </Switch>
       </div>
     </BrowserRouter>
