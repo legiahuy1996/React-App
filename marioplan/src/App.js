777b89def72b94afbc6c79ac766b4737bf199717
Huy <legiahuy1996@outlook.com>
2019-12-07 07:17:40 +0700

    Dashboard component

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/navbar'
+import Dashboard from './components/dashboard/Dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-          <Navbar></Navbar>
+        <Navbar />
+        <Switch>
+          <Route path='/' component={Dashboard}></Route>
+        </Switch>
       </div>
     </BrowserRouter>
   );
