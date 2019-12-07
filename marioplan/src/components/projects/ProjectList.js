e3c3d6d6a89bdcfe84d265f6859d26c0c36c64ae
Huy <legiahuy1996@outlook.com>
2019-12-07 07:22:23 +0700

    ProjectSummary component

--- marioplan/src/components/projects/ProjectList.js
+++ marioplan/src/components/projects/ProjectList.js
@@ -1,24 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
             <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project title</span>
-                    <p>Posted by Peter Parker</p>
-                    <p className="grey-text">7rd December, 7am</p>
-                </div>
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project title</span>
-                    <p>Posted by Peter Parker</p>
-                    <p className="grey-text">7rd December, 7am</p>
-                </div>
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project title</span>
-                    <p>Posted by Peter Parker</p>
-                    <p className="grey-text">7rd December, 7am</p>
-                </div>
+                <ProjectSummary />
+                <ProjectSummary />
+                <ProjectSummary />
             </div>
         </div>
     )
---
+++ marioplan/src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProjectSummary = () =>{
+    return (
+         <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project title</span>
+                    <p>Posted by Peter Parker</p>
+                    <p className="grey-text">7rd December, 7am</p>
+                </div>
+    )
+}
+
+export default ProjectSummary
