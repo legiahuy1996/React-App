0a0b25b92b4adc6f2ba396b2f8bff44c6737bb0b
Huy <legiahuy1996@outlook.com>
2019-12-08 10:07:15 +0700

    Using thunk

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -1,32 +1,31 @@
 import React, { Component } from 'react'
-import Notification from './Notification'
 import ProjectList from '../projects/ProjectList'
+import Notifications from './Notification'
 import { connect } from 'react-redux'
 
 class Dashboard extends Component {
-    render() {
-        // console.log(this.props);
-        const { projects } = this.props; 
-
-        return (
-            <div className="dashboard container">
-                <div className="row">
-                    <div className="col s12 m6">
-                        <ProjectList projects={projects}/>
-                    </div>
-                    <div className="col s12 m5 offset-m1">
-                        <Notification></Notification>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { projects } = this.props;
+    
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s12 m6">
+            <ProjectList projects={projects} />
+          </div>
+          <div className="col s12 m5 offset-m1">
+            <Notifications />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = (state, ownState) => {
-    return {
-        project: state.project.projects
-    }
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects
+  }
 }
 
 export default connect(mapStateToProps)(Dashboard)
