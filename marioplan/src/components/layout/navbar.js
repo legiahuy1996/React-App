1f37b46b7bf35cec0e3ee67df7add93e83adf2f6
Huy <legiahuy1996@outlook.com>
2019-12-11 14:12:15 +0700

    Tracking Auth status

--- marioplan/src/components/layout/navbar.js
+++ marioplan/src/components/layout/navbar.js
@@ -4,13 +4,14 @@ import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
+    const link = auth.uid ? <SignedInLinks></SignedInLinks> : <SignedOutLinks></SignedOutLinks>
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">MarioPlan</Link>
-                <SignedInLinks></SignedInLinks>
-                <SignedOutLinks></SignedOutLinks>
+                {link}
             </div>
         </nav>
     )
@@ -19,7 +20,7 @@ const Navbar = () => {
 const mapStateToProps = (state) => {
     console.log(state);
     return {
-
+        auth: state.firebase.auth
     }
 }
 
