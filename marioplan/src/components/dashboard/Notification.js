7737215e23e919f76fb30043742e4e90878b419a
Huy <legiahuy1996@outlook.com>
2019-12-14 22:03:46 +0700

    Cloud Function Auth Trigger

--- marioplan/src/components/dashboard/Notification.js
+++ marioplan/src/components/dashboard/Notification.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Notification = () => {
+const Notification = (props) => {
     return (
-        <div>
-            <p>Notifications</p>
+        <div className="section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <div className="card-title">Notifications</div>
+                    <ul className="notifications">
+                        <li>
+                            Notifications
+                        </li>
+                        <li>
+                            Notifications
+                        </li>
+                        <li>
+                            Notifications
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
